5c06819083fcb6c7813a2d5c506deb011b97a4fc
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2026-01-20 22:04:30 +0530

    Add files via upload

---
+++ Classwork/sync_4.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<unistd.h>              //library for read and write
+#include<fcntl.h>
+#include<string.h>
+#include<errno.h>
+
+int main()
+{
+    int fd=0;
+
+    char Buffer[]="indiaismycountry";
+    int iRet=0;
+
+    fd=open("Demo.txt",O_CREAT|O_WRONLY|O_APPEND,0777);
+
+    if(fd<0)
+    {
+        printf("Unable to open the file\n");
+        printf("Reason : %s\n",strerror(errno));
+        return -1;
+    }
+
+    printf("File succesfully opened with fd:%d\n",fd);
+
+    iRet=write(fd,Buffer,16);
+
+    printf("%d bytes gets succesfully written\n",iRet);
+
+    fsync(fd);
+
+    //Process is in waiting state;
+
+    close(fd);
+
+    
+
+    return 0;
+}

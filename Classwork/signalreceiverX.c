d0757aa6c48cc51e708c086f0838257ef8de8c71
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2026-03-30 14:33:45 +0530

    Add files via upload

---
+++ Classwork/signalreceiverX.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<signal.h>
+#include<unistd.h>
+
+void MarvellousHandler(int no)
+{
+    printf("Marvellous Message -Signal arrived %d\n",no);
+}
+
+int main()
+{
+    printf("PID is :%d\n",getpid());
+    signal(SIGUSR1,MarvellousHandler);
+
+    while(1)
+    {
+        sleep(1);
+    }
+    
+    return 0;
+}

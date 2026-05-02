e4ff8ac52998cc0f02bfae6bffe5faa14dd59ce1
Atharva Kailash Dhamale <atharvadhamale43@gmail.com>
2026-05-02 14:14:31 +0530

    Add files via upload

---
+++ Classwork/Dynamic/Server1.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<dlfcn.h>
+#include<stdlib.h>
+
+int main()
+{
+    void *handle=NULL;
+    int (*fp)(int,int);
+    int iRet=0;
+    
+
+    handle=dlopen("./libClient1.so",RTLD_LAZY);
+
+    if(handle==NULL)
+    {
+        printf("Unable to load the library \n");
+        return -1;
+    }
+
+    printf("Library gets loaded sucesfully\n");
+
+    fp=(int(*)(int,int))dlsym(handle,"Addition");
+
+    if(fp==NULL)
+    {
+        printf("Unable to get address of function\n");
+        dlclose(handle);
+        return -1;
+    }
+
+    iRet=fp(11,10);
+    printf("Addition is :%d\n",iRet);
+
+
+    fp=(int(*)(int,int))dlsym(handle,"Subtraction");
+
+    if(fp==NULL)
+    {
+        printf("Unable to get address of function\n");
+        dlclose(handle);
+        return -1;
+    }
+
+    iRet=fp(11,10);
+    printf("Subtraction is :%d\n",iRet);
+
+    dlclose(handle);
+
+
+    return 0;
+}
